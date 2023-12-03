36a5aa7845a8c92cb89d9ae15bf7168a9af44023
Zakki0925224 <zakki0925224@gmail.com>
2023-12-03 14:54:33 +0900

    [Update]: Write rom from file

--- src/nand2tetris/computer.sv
+++ src/nand2tetris/computer.sv
@@ -12,12 +12,17 @@ module computer(
     output wire[15:0] mmio_led
 );
     reg[15:0] inst;
+    reg[15:0] inst_rom [0:32767]; // 32KB
     wire[15:0] out_m, in_m;
     wire[15:0] pc, address_m;
     wire load_m;
 
+    initial begin
+        $readmemb("./rom/rom.hack", inst_rom);
+    end
+
     always @(posedge clk) begin
-        inst <= inst_rom(pc);
+        inst <= inst_rom[pc];
     end
 
     cpu cpu_(
@@ -42,20 +47,20 @@ module computer(
     );
 
     // rom32k
-    function[15:0] inst_rom;
-        input [15:0] addr;
-        begin
-            case (addr)
-                0:        inst_rom = 16'b0000_0000_0011_0101; // @53
-                1:        inst_rom = 16'b1110_1100_0001_0000; // D=A
-                2:        inst_rom = 16'b0100_0000_0000_0000; // @16384
-                3:        inst_rom = 16'b1110_0011_0000_1000; // M=D
-                4:        inst_rom = 16'b0000_0000_0000_0101; // @5
-                5:        inst_rom = 16'b1110_1010_1000_0111; // 0;JMP
-                default:  inst_rom = 16'b0000_0000_0000_0000;
-            endcase
-        end
-    endfunction
+    // function[15:0] inst_rom;
+    //     input [15:0] addr;
+    //     begin
+    //         case (addr)
+    //             0:        inst_rom = 16'b0000_0000_0011_0101; // @53
+    //             1:        inst_rom = 16'b1110_1100_0001_0000; // D=A
+    //             2:        inst_rom = 16'b0100_0000_0000_0000; // @16384
+    //             3:        inst_rom = 16'b1110_0011_0000_1000; // M=D
+    //             4:        inst_rom = 16'b0000_0000_0000_0101; // @5
+    //             5:        inst_rom = 16'b1110_1010_1000_0111; // 0;JMP
+    //             default:  inst_rom = 16'b0000_0000_0000_0000;
+    //         endcase
+    //     end
+    // endfunction
 
     assign debug_pc = pc;
     assign debug_inst = inst;
