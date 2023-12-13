cbc090b729b64e18acf537844f8fe5fc0e7a833f
Zakki0925224 <zakki0925224@gmail.com>
2023-12-13 20:58:58 +0900

    [Add]: Added RGB LCD module

--- src/controller/rgb_lcd.sv
+++ src/controller/rgb_lcd.sv
@@ -2,8 +2,6 @@
 
 module rgb_lcd
 #(
-    parameter DELAY_FRAMES = 3, // 27MHz to 9Mhz
-
     parameter H_ACTIVE = 16'd480,
     parameter H_FP = 16'd2,
     parameter H_SYNC = 16'd41,
@@ -31,8 +29,8 @@ module rgb_lcd
     output wire[9:0] h_pos, // current horizontal position
     output wire[9:0] v_pos  // current vertical position
 );
-    reg[11:0] h_cnt = 0;
-    reg[11:0] v_cnt = 0;
+    reg[11:0] h_cnt;
+    reg[11:0] v_cnt;
 
     reg hs_reg;
     reg vs_reg;
@@ -49,20 +47,38 @@ module rgb_lcd
     assign h_pos = h_pos_reg;
     assign v_pos = v_pos_reg;
 
+    // 27MHz to 9 MHz
     rgb_lcd_pll pll(
         .clkin(clk),
         .clkout(dclk)
     );
 
-    always @(posedge dclk) begin
-        h_cnt <= h_cnt + 1;
-
-        if ((h_cnt + 1) == H_TOTAL)
+    // h_cnt
+    always @(posedge dclk or posedge reset) begin
+        if (reset)
             h_cnt <= 0;
-            v_cnt <= v_cnt + 1;
 
-        if ((v_cnt + 1) == V_TOTAL)
+        else if ((h_cnt + 1) == H_TOTAL)
+            h_cnt <= 0;
+
+        else
+            h_cnt <= h_cnt + 1;
+    end
+
+    // v_cnt
+    always @(posedge dclk or posedge reset) begin
+        if (reset)
             v_cnt <= 0;
+
+        else if ((h_cnt + 1) == H_FP) begin
+            if ((v_cnt + 1) == V_TOTAL)
+                v_cnt <= 0;
+
+            else
+                v_cnt <= v_cnt + 1;
+        end
+        else
+            v_cnt <= v_cnt;
     end
 
     // hs
@@ -75,6 +91,9 @@ module rgb_lcd
 
         else if ((h_cnt + 1) == H_FP + H_SYNC)
             hs_reg <= ~hs_reg;
+
+        else
+            hs_reg <= hs_reg;
     end
 
     // vs
@@ -87,6 +106,9 @@ module rgb_lcd
 
         else if (((v_cnt + 1) == V_FP + V_SYNC) && ((h_cnt + 1) == H_FP))
             vs_reg <= ~vs_reg;
+
+        else
+            vs_reg <= vs_reg;
     end
 
     // he
@@ -99,6 +121,9 @@ module rgb_lcd
 
         else if ((h_cnt + 1) == H_TOTAL)
             he_reg <= 0;
+
+        else
+            he_reg <= he_reg;
     end
 
     // ve
@@ -111,18 +136,27 @@ module rgb_lcd
 
         else if (((v_cnt + 1) == V_TOTAL) && ((h_cnt + 1) == H_FP))
             ve_reg <= 0;
+
+        else
+            ve_reg <= ve_reg;
+
     end
 
     // h_pos
     always @(posedge dclk) begin
         if ((h_cnt + 1) >= H_FP + H_SYNC + H_BP)
             h_pos_reg <= (h_cnt - 1) - (H_FP[11:0] + H_SYNC[11:0] + H_BP[11:0]);
+
+        else
+            h_pos_reg <= h_pos_reg;
     end
 
     // v_pos
     always @(posedge dclk) begin
         if ((v_cnt + 1) >= V_FP + V_SYNC + V_BP)
             v_pos_reg <= (v_cnt - 1) - (V_FP[11:0] + V_SYNC[11:0] + V_BP[11:0]);
+        else
+            v_pos_reg <= v_pos_reg;
     end
 endmodule
 `default_nettype wire
--- src/top.sv
+++ src/top.sv
@@ -27,6 +27,17 @@ module top(
     assign lcd_g = lcd_rgb_data[10:5];
     assign lcd_b = lcd_rgb_data[4:0];
 
+    assign lcd_rgb_data = lcd_de ?
+        lcd_x <  30 ? 16'B10000_000000_00000: lcd_x <  60 ? 16'B01000_000000_00000:
+        lcd_x <  90 ? 16'B00100_000000_00000: lcd_x < 120 ? 16'B00010_000000_00000:
+        lcd_x < 150 ? 16'B00001_000000_00000: lcd_x < 180 ? 16'B00000_100000_00000:
+        lcd_x < 210 ? 16'B00000_010000_00000: lcd_x < 240 ? 16'B00000_001000_00000:
+        lcd_x < 270 ? 16'B00000_000100_00000: lcd_x < 300 ? 16'B00000_000010_00000:
+        lcd_x < 330 ? 16'B00000_000001_00000: lcd_x < 360 ? 16'B00000_000000_10000:
+        lcd_x < 390 ? 16'B00000_000000_01000: lcd_x < 420 ? 16'B00000_000000_00100:
+        lcd_x < 450 ? 16'B00000_000000_00010: 16'B00000_000000_00001
+        : 16'H0000;
+
     computer computer_(
         .clk(clk),
         .reset(reset),
@@ -45,7 +56,7 @@ module top(
 
     rgb_lcd lcd(
         .clk(clk),
-        .reset(reset),
+        .reset(0),
         .dclk(lcd_dclk),
         .de(lcd_de),
         .hs(lcd_hs),
@@ -53,10 +64,6 @@ module top(
         .h_pos(lcd_x),
         .v_pos(lcd_y)
     );
-
-    always @(posedge lcd_dclk) begin
-        lcd_rgb_data <= 16'h0;
-    end
 endmodule
 
 `default_nettype wire
