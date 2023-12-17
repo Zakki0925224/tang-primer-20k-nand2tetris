6a7c59c5dfa04a056e6d4dc42caa7901152e0862
Zakki0925224 <zakki0925224@gmail.com>
2023-12-17 17:01:26 +0900

    [Add]: Text VRAM can now be displayed

--- src/nand2tetris/ioport.sv
+++ src/nand2tetris/ioport.sv
@@ -42,11 +42,17 @@ module ioport(
     // RGB LCD display
     parameter LCD_HEIGHT = 272;
     parameter LCD_WIDTH = 480;
-    reg[15:0] lcd_vram[130559:0];
-    reg[15:0] lcd_vram_control_reg = 16'h0;
+    parameter LCD_TEXT_HEIGHT = 16;
+    parameter LCD_TEXT_WIDTH = 8;
+    reg[15:0] lcd_text_vram[(LCD_HEIGHT / LCD_TEXT_HEIGHT) * (LCD_WIDTH / LCD_TEXT_WIDTH) - 1:0] = '{default: 0}; // 1020B
+    reg[9:0] lcd_text_vram_pos;
+    reg[15:0] lcd_text_vram_data;
     wire[9:0] lcd_x, lcd_y;
     reg[15:0] lcd_rgb_data;
-    assign lcd_rgb_data = lcd_vram[(lcd_y * LCD_HEIGHT) + lcd_x];
+
+    assign lcd_text_vram_pos = ((LCD_WIDTH / LCD_TEXT_WIDTH) * (lcd_y / LCD_TEXT_HEIGHT)) + (lcd_x / LCD_TEXT_WIDTH); // ok
+    assign lcd_text_vram_data = lcd_text_vram[lcd_text_vram_pos];
+    assign lcd_rgb_data = (lcd_text_vram_data[7:0] != 0) && ((ascii_font[(LCD_TEXT_WIDTH * lcd_text_vram_data[7:0]) + (LCD_TEXT_WIDTH - (lcd_x % LCD_TEXT_WIDTH) - 1)] >> (lcd_y % LCD_TEXT_HEIGHT)) & 16'h1) ? 16'B10000_000000_00000 : 16'B00100_000000_00000;
     assign lcd_r = lcd_rgb_data[15:11];
     assign lcd_g = lcd_rgb_data[10:5];
     assign lcd_b = lcd_rgb_data[4:0];
@@ -62,6 +68,35 @@ module ioport(
         .v_pos(lcd_y)
     );
 
+    reg[15:0] ascii_font[128 * LCD_TEXT_WIDTH - 1:0] = '{default: 0};
+
+    initial begin
+        lcd_text_vram[0] <= 16'h0041;
+        lcd_text_vram[1] <= 16'h0042;
+        lcd_text_vram[61] <= 16'h0041;
+        lcd_text_vram[62] <= 16'h0042;
+
+        // 'A'
+        ascii_font[8 * 65 + 7] <= 16'h0000;
+        ascii_font[8 * 65 + 6] <= 16'h1fc0;
+        ascii_font[8 * 65 + 5] <= 16'h0220;
+        ascii_font[8 * 65 + 4] <= 16'h0220;
+        ascii_font[8 * 65 + 3] <= 16'h0220;
+        ascii_font[8 * 65 + 2] <= 16'h1fc0;
+        ascii_font[8 * 65 + 1] <= 16'h0000;
+        ascii_font[8 * 65 + 0] <= 16'h0000;
+
+        // 'B'
+        ascii_font[8 * 66 + 7] <= 16'h0000;
+        ascii_font[8 * 66 + 6] <= 16'h1fe0;
+        ascii_font[8 * 66 + 5] <= 16'h1120;
+        ascii_font[8 * 66 + 4] <= 16'h1120;
+        ascii_font[8 * 66 + 3] <= 16'h1120;
+        ascii_font[8 * 66 + 2] <= 16'h0ec0;
+        ascii_font[8 * 66 + 1] <= 16'h0000;
+        ascii_font[8 * 66 + 0] <= 16'h0000;
+    end
+
     always @(posedge clk) begin
         // LED
         if (address == 16'h4000 && load)
@@ -73,22 +108,16 @@ module ioport(
             io_uart_update <= ~io_uart_update;
         end
 
-        // VRAM control register
-        else if (address == 16'h807e && load) begin
-            lcd_vram_control_reg <= in;
-        end
-
-        // VRAM
+        // Text VRAM
         else if (address >= 16'h807f && load) begin
-            lcd_vram[32640 * lcd_vram_control_reg[1:0] + (address - 16'h807f)] <= in;
+            lcd_text_vram[address - 16'h807f] <= in;
         end
     end
 
     assign out =
         address ==  16'h4000 ?  io_led :
         address ==  16'h4001 ? io_uart :
-        address ==  16'h807e ? lcd_vram_control_reg :
-        address >= 16'h807f ? lcd_vram[32640 * lcd_vram_control_reg[1:0] + (address - 16'h807f)] :
+        address >= 16'h807f ? lcd_text_vram[address - 16'h807f] :
         16'h0;
 
 endmodule
